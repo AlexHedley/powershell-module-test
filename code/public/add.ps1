13a24f8b73d0c42d61654504b54c8db3edcd0ec5
Alex Hedley <alex.hedley@me.com>
2019-12-31 14:14:32 +0000

    Added Subtrack-Numbers function

--- code/public/add.ps1
+++ code/public/add.ps1
@@ -13,6 +13,21 @@ Function Add-Numbers($one, $two) {
     $one + $two
 }
 
+Function Subtrack-Numbers($one, $two) {
+    <#
+    .PARAMETER one
+        The first number
+
+    .PARAMETER two
+        The second number
+
+
+    .EXAMPLE
+        Subtrack-Numbersy -one 1 -two 2
+  #>
+    $one - $two
+}
+
 Function Get-PowerShellProcess {
     Get-Process PowerShell
 }
